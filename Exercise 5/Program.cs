e684ff8e331b23891c3cec4ebd967762174570d1
DzakyNaufal <dzakynmulna@gmail.com>
2022-12-21 16:41:18 +0700

    membuat kelas node

--- Exercise 5/Program.cs
+++ Exercise 5/Program.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace Exercise_5
 {
-    class Program
+    class Node 
     {
-        int dzaky, naufal, max = 5;
-        int[] queue_array = new int[5];
-        public Program()
-        {
-            dzaky = -1;
-            naufal = -1;
-        }
-
-
+        public string nama;
+        public Node next;
+       
+    }
+    
     }
 }
